923d48228abea485d82d7e7b617309a5002b57f2
Antonis Berkakis <antonis.berkakis@simplybusiness.co.uk>
2017-09-12 18:04:35 +0100

    Set up dynamic ejs
    Signed-off-by: Antonis Berkakis <antonis.berkakis@simplybusiness.co.uk>

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
 const express = require('express'),
     app = express();
 
+app.set('view engine', 'ejs');
 
 app.get("/", (req, res) => {
-  res.send("Hello world 2.0!");
+  //res.send("Hello world 2.0!");
+  res.render('blank', {
+    title: "Mastering Flux",
+    body: "Hello world 3.1"
+  })
 });
 
 
